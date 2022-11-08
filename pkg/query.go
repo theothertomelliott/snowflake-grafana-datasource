7b28d7e5aebc45860ba44a22c37b9fe4e8597ae9
Tom Elliott <telliott@yext.com>
2022-11-08 13:50:52 -0500

    Add nil checks for column values
    This fixes occasional "Query data error" errors on queries.

--- pkg/query.go
+++ pkg/query.go
@@ -145,12 +145,18 @@ func (qc *queryConfigStruct) transformQueryResult(columnTypes []*sql.ColumnType,
 
 		switch column_types[i].ScanType() {
 		case reflect.TypeOf(boolean):
-			values[i] = values[i].(bool)
+			if values[i] != nil {
+				values[i] = values[i].(bool)
+			}
 		case reflect.TypeOf(tim):
-			values[i] = values[i].(time.Time)
+			if values[i] != nil {
+				values[i] = values[i].(time.Time)
+			}
 		case reflect.TypeOf(integer):
 			n := new(big.Float)
-			n.SetString(values[i].(string))
+			if values[i] != nil {
+				n.SetString(values[i].(string))
+			}
 			precision, _, _ := columnTypes[i].DecimalSize()
 			if precision > 1 {
 				values[i], _ = n.Float64()
